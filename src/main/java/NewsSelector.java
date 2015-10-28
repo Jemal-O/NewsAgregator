6f421ca750276efec705ee3e1b6f41de87fff568
Dzhemal Orazgeldyeva <Dzhemal_Orazgeldyeva@epam.com>
2015-10-28 15:44:08 +0300

    nothing

--- src/main/java/NewsSelector.java
+++ src/main/java/NewsSelector.java
@@ -10,6 +10,7 @@ import java.util.Enumeration;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
