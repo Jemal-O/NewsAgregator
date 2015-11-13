7c8a18528200d3c127716eb22eae93b619a94337
Dzhemal Orazgeldyeva <Dzhemal_Orazgeldyeva@epam.com>
2015-11-13 14:05:19 +0300

    delete spaces

--- src/main/java/container/FoldeAdditionServlet.java
+++ src/main/java/container/FoldeAdditionServlet.java
@@ -2,7 +2,6 @@ package container;
 
 import java.io.IOException;
 import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
